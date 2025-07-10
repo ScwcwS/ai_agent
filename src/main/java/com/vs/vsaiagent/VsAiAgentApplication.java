b152c16e6d56d335b20ee32598ce6edbb03f3d78
Scw <134371410+ScwcwS@users.noreply.github.com>
2025-07-10 18:54:14 +0800

    ai agent

---
+++ src/main/java/com/vs/vsaiagent/VsAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.vs.vsaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsAiAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VsAiAgentApplication.class, args);
+	}
+
+}
